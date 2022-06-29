99792b058f8a08f1205e412572930d7907d16f2a
Bryan Wu <bryanwu@berkeley.edu>
2022-06-28 20:18:21 -0700

    add new file structure

---
+++ Code/common/constants.py
@@ -0,0 +1,57 @@
+import datetime
+
+# Paths to things
+dir_drive_home ='drive/MyDrive/MV1012_SBB_images/ML_projects'
+dir_drive_data = f'{dir_drive_home}'
+dir_google_cloud = 'gs://paleo-ml'
+dir_data = f'{dir_google_cloud}/Processed_data/forams'
+dir_training_logs = f'{dir_google_cloud}/Training_logs/'
+dir_trained_models = f'{dir_drive_home}/Trained_models/'
+dir_train_data = f'{dir_data}/train'
+dir_val_data = f'{dir_data}/val'
+dir_test_data = f'{dir_data}/test'
+
+# Info about data
+image_size = (416, 416)
+input_shape = (*image_size, 3)
+num_classes = 65
+
+# default training config
+default_training_config = { # lol thanks copilot lemme take a look at what this is
+    'epochs': 10,
+    'batch_size': 128*2,
+}
+#     'learning_rate': 1e-4,
+#     'steps_per_epoch': None,
+#     'validation_steps': None,
+#     'validation_freq': 1,
+#     'callbacks': [
+#         'tensorboard',
+#         'checkpoint',
+#         'early_stopping',
+#         'reduce_lr',
+#         'terminate_on_nan',
+#     ],
+#     'tensorboard': {
+#         'log_dir': f'{dir_training_logs}/{datetime.now().strftime("%Y%m%d-%H%M%S")}',
+#     },
+#     'checkpoint': {
+#         'filepath': f'{dir_trained_models}/checkpoint-{datetime.now().strftime("%Y%m%d-%H%M%S")}.h5',
+#         'save_best_only': True,
+#         'save_weights_only': True,
+#         'verbose': 1,
+#     },
+#     'early_stopping': {
+#         'patience': 10,
+#         'verbose': 1,
+#     },
+#     'reduce_lr': {
+#         'factor': 0.1,
+#         'patience': 5,
+#         'min_lr': 1e-8,
+#         'verbose': 1,
+#     },
+#     'terminate_on_nan': {
+#         'verbose': 1,
+#     },
+# }
---
+++ Code/data_processing/data_processor.py
@@ -0,0 +1,112 @@
+import os, sys, re, math
+sys.path.append(os.getcwd() + '/..')
+from common.constants import *
+
+
+from data_processing import *
+import pandas as pd
+import numpy as np
+np.random.seed(2022)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import os, re, math
+import keras
+import shutil
+from pathlib import Path
+from PIL import Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+def create_training_data(from_file_path, labels_df, data_dir, override, verbose=False):
+    """
+    Takes a single image file FROM_FILE_PATH and crops and resizes it
+     to create a training data image. Write that to the corresponding directory.
+    """
+    
+    ## Extract identifying information from the sample ##
+    # from_file_path == drive/MyDrive/MV1012_SBB_images/Box_Core_images/MV1012-BC-8_identify/MV1012-BC-8_obj01142_plane000.jpg
+
+    # MV1012-BC-8_obj01142.jpg
+    image_file_name = os.path.basename(from_file_path).replace('_plane000', '')
+
+    # MV1012-BC-8_obj01142
+    image_name = Path(image_file_name).stem
+
+    # MV1012-BC-8 01142
+    sample_name, object_number = re.match(r'(.+)_obj(\d+)', image_name).groups()
+    object_number = int(object_number)
+    
+    # TODO: exit if sample_name and object_number now found within labels_df
+    if not (sample_name, object_number) in labels_df.index:
+        if verbose: print(f'skipped {(sample_name, object_number)} becuase it is not in labels_df')
+        return
+
+    # can get label after confirming existence
+    species_label = labels_df.loc[(sample_name, object_number), 'species']
+    dataset_type = 'train'
+    if labels_df.loc[(sample_name, object_number), 'test']:
+        dataset_type = 'test'
+    elif labels_df.loc[(sample_name, object_number), 'val']:
+        dataset_type = 'val'
+
+
+    ## Create empty dir or exit if it already exists and we don't want to override.
+    img_location = os.path.join(data_dir, dataset_type, image_file_name)
+    print(img_location)
+    if os.path.exists(img_location):
+        if not override: 
+            if verbose: print(f'skipped {image_file_name}')
+            return
+        os.remove(img_location)
+
+    img = Image.open(from_file_path)
+    img_data = np.asarray(img)
+    label_size = 160
+    no_label_img_data = img_data[:-label_size]
+
+    ## Find columns where a majority of the summed pixel intensities is 0
+    zero_pixels = no_label_img_data.sum(axis=(2)) == 0
+    col_filter = zero_pixels.sum(axis=0) < zero_pixels.shape[0]/5 #Cols where less than 1/5 pixels are zeros
+    cropped_image_data = no_label_img_data[:, col_filter, :]
+    cropped_image = Image.fromarray(cropped_image_data)
+    ## Convert to image again and resize
+    size = 416 # input size, width and height of image 
+    resized_image = cropped_image.resize((size, size))
+
+    ## Store image in output directory
+    # print(os.path.join(data_point_dir, f'{image_name}.jpg'))
+    resized_image.save(img_location)
+    if verbose: print(f'created ${image_name} in {species_label} as {dataset_type}')
+
+
+def process_sample_dir(sample_dir, sample_name, labels_df, data_dir=dir_dev_data, override=False, verbose=True):
+    '''
+    Takes a directory full of images and loops over them 
+    to process the images.
+    '''
+    for file_name in os.listdir(sample_dir):
+        file_path = os.path.join(sample_dir, file_name)
+        file_ext = os.path.splitext(file_name)[-1].lower()
+        if file_ext == '.csv':
+            pass
+        elif file_ext == '.jpg':
+            #TODO: possible make the name the classification? 
+            create_training_data(file_path, labels_df, data_dir=data_dir,override=override, verbose=verbose) #TODO set training data dir instead of using default debug
+
+def purge(train_dir):
+    print(f'purging all images in {train_dir}. Type "YES" to continue.')
+    if input() != "YES":
+        return
+    for class_dir in os.listdir(train_dir):
+        print(f'starting {class_dir}')
+        total_deleted_in_class = 0
+        class_path = os.path.join(train_dir, class_dir)
+        for file in os.listdir(class_path):
+            total_deleted_in_class += 1
+            file_path = os.path.join(class_path, file)
+            try:
+                os.remove(file_path)
+            except:
+                shutil.rmtree(file_path)
+        print(f'finished {class_dir} and removed {total_deleted_in_class}')
