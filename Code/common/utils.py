6509163d2299e467b002802102815ca9a5786b0a
Bryan Wu <bryanwu@berkeley.edu>
2023-01-03 17:03:10 -0800

    download dir

--- Code/common/utils.py
+++ Code/common/utils.py
@@ -12,3 +12,27 @@ def rename_cloud_file(original_name, new_name):
     bucket = storage_client.bucket('paleo-ml')
     blob = bucket.blob(original_name)
     new_blob = bucket.rename_blob(blob, new_name)
+
+def download_dir(dir_name):
+    """
+    Download a directory from Google Cloud Storage.
+    Args:
+        dir_name (str): The name of the directory to download.
+    Returns:
+        str: The path to the downloaded directory.
+    """
+    # The ID of your GCS bucket
+    bucket_name = "paleo-ml"
+    # The ID of the GCS object to download
+    # blob_name = "your-object-name"
+    # The path to which the file should be downloaded
+    # download_to_filename = "local/path/to/file"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=dir_name)
+    for blob in blobs:
+        if blob.name.endswith('/'):
+            continue
+        blob.download_to_filename(blob.name)
+    return dir_name
